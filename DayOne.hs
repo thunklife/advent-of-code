6633c6578b4852bd43266005c368b84ac77c2c5f
Jesse Williamson <jessewilliamson@fastmail.fm>
2016-01-11 22:15:56 -0800

    day one done

--- DayOne.hs
+++ DayOne.hs
@@ -8,7 +8,7 @@ parenToInt ')' = -1
 parenToInt _   = 0
 
 solution1 :: String -> Int
-solution1 = sum . fmap f
+solution1 = sum . fmap parenToInt
 
 solution2 :: String -> Int
-solution2 = length . head . filter ((== -1) . sum) . drop 1 . inits . fmap f
+solution2 = length . head . filter ((== -1) . sum) . drop 1 . inits . fmap parenToInt
